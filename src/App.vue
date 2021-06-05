5f49f0624a179622ac6d8f59ccb51588eb7fc9e2
Nikita Derevskov <nikitaderevskov@gmail.com>
2021-06-05 18:42:13 +0300

    add second component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <UseGetter />
+    <DoNotUseGetter />
   </div>
 </template>
 
 <script>
 import UseGetter from './components/UseGetter.vue'
+import DoNotUseGetter from './components/DoNotUseGetter.vue'
 
 export default {
   name: 'App',
   components: {
-    UseGetter
+    UseGetter,
+    DoNotUseGetter
   }
 }
 </script>
---
+++ src/components/DoNotUseGetter.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    I do not use getter
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "DoNotUseGetter"
+}
+</script>
