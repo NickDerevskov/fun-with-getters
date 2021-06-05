36d5ab532caea49f039297f0bf3a4de2e5f28d54
Nikita Derevskov <nikitaderevskov@gmail.com>
2021-06-05 18:40:24 +0300

    change after create

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UseGetter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UseGetter from './components/UseGetter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UseGetter
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
